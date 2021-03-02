ee9a1703bd78268510e3171571b9bfe3e85e1c1e
George Mavridis <g.mavridis@pan-net.eu>
2021-03-02 16:46:06 +0200

    Adding files.

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,3 @@
-module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/covid-19-dashboard/" : "/",
-};
-
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
   .BundleAnalyzerPlugin;
 module.exports = {
